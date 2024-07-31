d56d2752b213e00024be1cfcb4c20226980d9e7d
Rafael Monteiro <rmxvmail@gmail.com>
2024-07-31 20:17:49 -0300

    A colisão do inimigo já está sendo renderizada no mapa de colisão.

--- codigo1.asm
+++ codigo1.asm
@@ -1,17 +1,17 @@
 .data
-enemy1Pos: .word 32, 16
-enemy1Intention: .byte 0
-enemy1Move: .byte 0
-playerIntention: .byte 0
 playerMove: .byte 0
+playerIntention: .byte 0
 playerPos: .word 16, 16
+playerLastPos: .word 16, 16
+
+enm1Move: .byte 0
+enm1Intention: .byte 0
+enm1Pos: .word 32, 16
+enm1LastPos: .word 32, 16
+
 counterNaoMexe: .byte 0
-lastPlayerPos: .word 16, 16
-lastEnemy1Pos: .word 32, 16
-enemy2Pos: .word 64, 16
-lastEnemy2Pos: .word 64, 16
-enemy2Intention: .byte 0
-enemy2Move: .byte 0
+
+fnMem1: .word 0,0,0,0,0,0,0,0
 
 .include "mapa2.data"
 .include "bloco.data"
@@ -20,17 +20,27 @@ enemy2Move: .byte 0
 
 .text
 
+#renderiza o mapa
 la a0, mapa2
 addi a0, a0, 8
-jal, mapRender
+jal mapRender
 
+#renderiza a colisão inicial do inimigo 1
+la a0, colisao
+addi a0, a0, 8
+la a1, enm1Pos
+lw a2, 4(a1)
+lw a1, 0(a1)
+li a3, 123
+jal solidColorTileRender
+
+#início do game loop
 start:
 
 #checa certos casos especiais
 la a0, playerPos
 jal checkLeftEnd
 jal checkRightEnd
-
 
 la t0, counterNaoMexe
 lb t0, 0(t0)
@@ -41,54 +51,75 @@ bne t0, zero, EP26
 
 	
 	#define a movimentação do player
-	jal playerMovement
-
-
-	#define a movimentação do inimigo
-	la a0, enemy1Intention
-	la a1, enemy1Move
-	la a2, enemy1Pos
-	jal enemyMovement
+	la a0, playerIntention
+	la a1, playerMove
+	la a2, playerPos
+	la a3, colisao
+	addi a3, a3, 8
+	jal entityMove
 
 EP26:
 
-#movimenta o inimigo
-la t0, enemy1Move
-la t1, enemy1Pos
-jal changeEnemyPos
+#define a movimentação do inimigo
+la a0, enm1Intention
+la a1, enm1Move
+la a2, enm1Pos
+la a3, colisao
+addi a3, a3, 8
+jal entityMove
 
-#movimenta o player
-jal changePlayerPos
+#altera a posição do player
+la a0, playerPos
+la a1, playerLastPos
+la a2, playerMove
+jal changeEntityPos
 
-# desrenderiza o inimigo
-la t1, lastEnemy1Pos
-lw a0, 0(t1)
-lw a1, 4(t1)
-la a2, mapa2
-addi a2, a2, 8
-jal tileUnrender
+#altera a posição do inimigo
+la a0, enm1Pos
+la a1, enm1LastPos
+la a2, enm1Move
+jal changeEntityPos
+
+#renderiza a colisão do inimigo 1
+la a0, colisao
+addi a0, a0, 8 
+la a1, enm1Pos
+lw a2, 4(a1)
+lw a1, 0(a1)
+la a3, enm1Move
+lb a3, 0(a3)
+li a4, 123
+jal moveEntityCollision
 
 # desrenderiza o player
-la t1, lastPlayerPos
+la t1, playerLastPos
 lw a0, 0(t1)
 lw a1, 4(t1)
 la a2, mapa2
 addi a2, a2, 8
 jal tileUnrender
 
-# renderiza o inimigo
-la t0, enemy1Pos
-lw a0, 0(t0)
-lw a1, 4(t0)
-la a2, gato1
+#desrenderiza o inimigo
+la t1, enm1LastPos
+lw a0, 0(t1)
+lw a1, 4(t1)
+la a2, mapa2
 addi a2, a2, 8
-jal tileRender
+jal tileUnrender
 
 # renderiza o player
 la t0, playerPos
 lw a0, 0(t0)
 lw a1, 4(t0)
 la a2, bloco
+addi a2, a2, 8
+jal tileRender
+
+#renderiza o inimigo
+la t0, enm1Pos
+lw a0, 0(t0)
+lw a1, 4(t0)
+la a2, gato1
 addi a2, a2, 8
 jal tileRender
 
@@ -108,12 +139,10 @@ EP27:
 jal start
 end:
 
-#definição de funções:
-
-##################
+##################################################
 # a0 -> o endereço de memória do primeiro pixel do 
 # mapa a ser renderizado
-##################
+##################################################
 
 mapRender:
 mv s6, ra
@@ -121,22 +150,18 @@ li s0, 76800 #1.
 li s1, 0xff000000 #2.
 li t1, 0
 LP1:
-bge t1, s0, LE1
-#---------------
-add t0, a0, t1
-lb t3, 0(t0)
-add t0, s1, t1
-sb t3, 0(t0)
-#---------------
-addi t1, t1, 1
-jal LP1 
+	bge t1, s0, LE1
+	#---------------
+	add t0, a0, t1
+	lb t3, 0(t0)
+	add t0, s1, t1
+	sb t3, 0(t0)
+	#---------------
+	addi t1, t1, 1
+	jal LP1 
 LE1:
 mv ra, s6
 ret
-
-#1. numero de pixeis
-#2. #endereço da tela
-
 
 
 ###########################################
@@ -193,7 +218,6 @@ ret
 # a2 -> endereço do primeiro pixel do mapa
 ###########################################
 
-
 tileUnrender:
 mv s6, ra
 li s0, 16
@@ -233,6 +257,10 @@ ret
 #1. endereço da tela
 #2. posição do primeiro pixel em relativo ao mapa ou à tela
 
+################################################################################
+#lê a tecla pressionada pelo jogador e define a "playerIntention" a partir disso
+################################################################################
+
 readKeyboard:
 mv s6, ra
 li s0, 0xff200000
@@ -268,237 +296,9 @@ EP1:
 mv ra, s6
 ret
 
-
-playerMovement:
-mv s7, ra
-la s0, playerIntention
-lb s0, 0(s0)
-mv a0, s0
-jal CheckMapCollision
-beq a0, zero, EP14
-	la s1, playerMove
-	la s0, playerIntention
-	lb s0, 0(s0)
-	sb s0, 0(s1)
-	mv ra, s7
-	ret
-EP14:
-
-la a0, playerMove
-lb a0, 0(a0)
-jal CheckMapCollision
-beq a0, zero, EP15
-	mv ra, s7
-	ret
-EP15:
-la a0, playerMove
-lb a0, 0(a0)
-jal rotateClock
-jal CheckMapCollision
-beq a0, zero, EP18
-	la s1, playerMove
-	lb a0, 0(s1)
-	jal rotateClock
-	sb a0, 0(s1)
-	la s1, playerIntention
-	sb a0, 0(s1)
-	mv ra, s7
-	ret
-EP18:
-
-
-la a0, playerMove
-lb a0, 0(a0)
-jal rotateCounter
-jal CheckMapCollision
-beq a0, zero, EP19
-	la s1, playerMove
-	lb a0, 0(s1)
-	jal rotateCounter
-	sb a0, 0(s1)
-	la s1, playerIntention
-	sb a0, 0(s1)
-	mv ra, s7
-	ret
-EP19:
-
-la a0, playerMove
-lb a0, 0(a0)
-jal rotateClock
-jal rotateClock
-mv s10, a0
-jal CheckMapCollision
-beq a0, zero, EP24
-	la s1, playerMove
-	sb s10, 0(s1)
-	la s1, playerIntention
-	sb s10, 0(s1)
-	mv ra, s7
-	ret
-EP24:
-
-mv ra, s7
-ret
-
-###############################################
-# a0 <- recebe a direção de checagem da colisão
-###############################################
-# retorna 1 no a 0 caso o caminho não esteja obstruido
-
-CheckMapCollision: #TODO: comentar essa porra dessa função
-mv s6, ra
-la s0, playerPos
-lw s1, 0(s0) # posição x do player
-lw s2, 4(s0) # posição y do player
-la s0, colisao
-addi s0, s0, 8
-
-li s4, 255 
-
-li t0, 320
-mul s3, s2, t0
-add s3, s3, s1
-add s3, s3, s0 # esse é o endereço do player no mapa de colisão
-
-
-bne a0, zero, EP6
-	addi t3, s3, 16
-	lb t4, 0(t3)
-	bne t4, zero, EP10
-		li t6, 4816
-		add t3, s3, t6
-		lb t4, 0(t3)
-		bne t4, zero, EP10
-			li a0, 1
-			mv ra, s6
-			ret
-	EP10:
-	mv a0, zero
-	mv ra, s6
-	ret
-EP6:
-
-
-li t0, 1
-bne a0, t0, EP7
-	addi t3, s3, -320
-	lb t4, 0(t3)
-	bne t4, zero, EP11
-		addi t3, s3, -305
-		lb t4, 0(t3)
-		bne t4, zero, EP11
-			li a0, 1
-			mv ra, s6
-			ret
-		
-	EP11:
-	mv a0, zero
-	mv ra, s6
-	ret
-EP7:
-
-
-li t0, 2
-bne a0, t0, EP8
-	addi t3, s3, -1
-	lb t4, 0(t3)
-	bne t4, zero, EP12
-		li t6, 4799
-		add t3, s3, t6
-		lb t4, 0(t3)
-		bne t4, zero, EP12
-			li a0, 1
-			mv ra, s6
-			ret
-	EP12: 
-	mv a0, zero
-	mv ra, s6
-	ret
-EP8:
-
-
-li, t0, 3
-bne a0, t0, EP9
-	li t6, 5120
-	add t3, s3, t6
-	lb t4, 0(t3)
-	bne t4, zero, EP13
-		li t6, 5135
-		add t3, s3, t6
-		lb t4, 0(t3)
-		bne t4, zero, EP13
-			li a0, 1
-			mv ra, s6
-			ret
-	EP13:
-	mv a0, zero
-	mv ra, s6
-	ret
-EP9:
-
-# você coloca um número (n) a0 e a função retorna (n+1) se (n < 3) e (0) se (n == 3)
-rotateClock:
-mv s6, ra
-li t0, 3
-bne t0, a0, EP16
-	mv a0, zero
-	mv ra, s6
-	ret
-EP16:
-addi a0, a0, 1
-mv ra, s6
-ret
-
-# você coloca um número (n) a0 e a função retorna (n-1) se (n > 0) e (3) se (n == 0)
-rotateCounter:
-mv s6, ra
-li t0, 3
-bne zero, a0, EP17
-	mv a0, t0
-	mv ra, s6
-	ret
-EP17:
-addi a0, a0, -1
-mv ra, s6
-ret
-
-changePlayerPos:
-mv s6, ra
-la s10, lastPlayerPos
-la s0, playerPos
-la s3, playerMove
-lb s3, 0(s3)
-lw s1, 0(s0)
-lw s2, 4(s0)
-sw s1, 0(s10)
-sw s2, 4(s10)
-li t0, 0
-bne s3, t0, EP20
-	addi s1, s1, 1
-	sw s1, 0(s0)
-EP20:
-
-li t0, 1
-bne s3, t0, EP21
-	addi s2, s2, -1
-	sw s2, 4(s0)
-EP21:
-
-li t0, 2
-bne s3, t0, EP22
-	addi s1, s1, -1
-	sw s1, 0(s0)
-EP22:
-
-li t0, 3
-bne s3, t0 EP23
-	addi s2, s2, 1
-	sw s2, 4(s0)
-EP23:
-mv ra, s6
-ret
-
-#a0 <- passa o endereço posição da entidade a se checar
+##########################################################
+#a0 <- recebe o endereço da posição da entidade a se checar
+##########################################################
 
 checkLeftEnd:
 mv s7, ra
@@ -522,12 +322,12 @@ bne s1, zero, EP25
 	mv a2, t1
 	jal tileUnrender
 EP25:
-
 mv ra, s7
 ret
 
-
-#a0 <- passa o endereço posição da entidade a se checar
+##########################################################
+#a0 <- recebe o endereço da posição da entidade a se checar
+##########################################################
 
 checkRightEnd:
 mv s7, ra
@@ -554,199 +354,382 @@ EP28:
 mv ra, s7
 ret
 
+####################################################
+#a0 <- endereço da intenção da entidade
+#a1 <- endereço da movimentação da entidade
+#a2 <- endereço da posição da entidade
+#a3 <- endereço do primeiro pixel do mapa de colisão
+####################################################
 
-#a1 <- posiçãoInimigo
+entityMove:
+la s11, fnMem1 #endereço da memória da função
+sw ra, 0(s11)
+sw a0, 4(s11) #endereço intenção
+sw a1, 8(s11) #endereço movimento
+sw a2, 12(s11) #endereço posição
+sw a3, 16(s11) #endereço do mapa colisão
 
-CheckEnemyMapCollision: #TODO: comentar essa porra dessa função
+lb a1, 0(a0)
+mv a0, a2
+mv a2, a3
+jal checkMapCollision
+beq a0, zero, EP14
+	lw s1, 8(s11)
+	lw s2, 4(s11)
+	lb s2, 0(s2)
+	sb s2, 0(s1)
+	lw ra, 0(s11)
+	ret
+EP14:
+
+lw a0, 12(s11)
+lw a1, 8(s11)
+lb a1, 0(a1)
+lw a2, 16(s11)
+jal checkMapCollision
+beq a0, zero, EP15
+	lw ra, 0(s11)
+	ret
+EP15:
+
+lw a0, 8(s11)
+lb a0, 0(a0)
+jal rotateClock
+mv a1, a0
+sb a1, 20(s11)
+lw a0, 12(s11)
+lw a2, 16(s11)
+jal checkMapCollision
+beq a0, zero, EP16
+	lb t0, 20(s11)
+	lw t1, 8(s11)
+	lw t2, 4(s11)
+	sb t0, 0(t1)
+	sb t0, 0(t2)
+	lw ra, 0(s11)
+	ret
+EP16:
+
+lw a0, 8(s11)
+lb a0, 0(a0)
+jal rotateCounter
+mv a1, a0
+sb a1, 20(s11)
+lw a0, 12(s11)
+lw a2, 16(s11)
+jal checkMapCollision
+beq a0, zero, EP17
+	lb t0, 20(s11)
+	lw t1, 8(s11)
+	lw t2, 4(s11)
+	sb t0, 0(t1)
+	sb t0, 0(t2)
+	lw ra, 0(s11)
+	ret
+EP17:
+
+lb a0, 20(s11)
+jal rotateCounter
+lw t1, 8(s11)
+lw t2, 4(s11)
+sb a0, 0(t1)
+sb a0, 0(t2)
+lw ra, 0(s11)
+ret
+
+#####################################################
+#a0 <- recebe o endereço da posição da entidade
+#a1 <- recebe a direção da checagem
+#a2 <- endereço do primeiro pixel do mapa de colisão
+#####################################################
+
+checkMapCollision:
 mv s6, ra
-mv s0, a1
-lw s1, 0(s0) # posição x do inimigo
-lw s2, 4(s0) # posição y do inimigo
-la s0, colisao
-addi s0, s0, 8
-
-li s4, 255 
+lw s1, 0(a0) #posição x da entidade
+lw s2, 4(a0) #posição y da entidade
 
 li t0, 320
 mul s3, s2, t0
 add s3, s3, s1
-add s3, s3, s0 # esse é o endereço do player no mapa de colisão
+add s3, s3, a2 # endereço da entidade no mapa de colisão
 
-bne a0, zero, EP33
-	addi t3, s3, 16
-	lb t4, 0(t3)
-	bne t4, zero, EP34
-		li t6, 4816
-		add t3, s3, t6
-		lb t4, 0(t3)
-		bne t4, zero, EP34
+
+bne a1, zero, EP6
+	addi t1, s3, 16
+	lb t2, 0(t1)
+	bne t2, zero, EP10
+		li t3, 4816
+		add t1, s3, t3
+		lb t2, 0(t1)
+		bne t2, zero, EP10
 			li a0, 1
 			mv ra, s6
 			ret
-	EP34:
+	EP10:
 	mv a0, zero
 	mv ra, s6
 	ret
-EP33:
-
+EP6:
 
 li t0, 1
-bne a0, t0, EP35
-	addi t3, s3, -320
-	lb t4, 0(t3)
-	bne t4, zero, EP36
-		addi t3, s3, -305
-		lb t4, 0(t3)
-		bne t4, zero, EP36
+bne a1, t0, EP7
+	addi t1, s3, -320
+	lb t2, 0(t1)
+	bne t2, zero, EP11
+		li t3, -305
+		add t1, s3, t3
+		lb t2, 0(t1)
+		bne t2, zero, EP11
 			li a0, 1
 			mv ra, s6
 			ret
-		
-	EP36:
+	EP11:
 	mv a0, zero
 	mv ra, s6
 	ret
-EP35:
-
+EP7:
 
 li t0, 2
-bne a0, t0, EP37
-	addi t3, s3, -1
-	lb t4, 0(t3)
-	bne t4, zero, EP38
-		li t6, 4799
-		add t3, s3, t6
-		lb t4, 0(t3)
-		bne t4, zero, EP38
+bne a1, t0, EP8
+	addi t1, s3, -1
+	lb t2, 0(t1)
+	bne t2, zero, EP12
+		li t3, 4799
+		add t1, s3, t3
+		lb t2, 0(t1)
+		bne t2, zero, EP12
 			li a0, 1
 			mv ra, s6
 			ret
-	EP38: 
+	EP12:
 	mv a0, zero
 	mv ra, s6
 	ret
-EP37:
+EP8:
 
-
-li, t0, 3
-bne a0, t0, EP39
-	li t6, 5120
-	add t3, s3, t6
-	lb t4, 0(t3)
-	bne t4, zero, EP40
-		li t6, 5135
-		add t3, s3, t6
-		lb t4, 0(t3)
-		bne t4, zero, EP40
+li t0, 3
+bne a1, t0, EP9
+	li t3, 5120
+	add t1, s3, t3
+	lb t2, 0(t1)
+	bne t2, zero, EP13
+		li t3, 5135
+		add t1, s3, t3
+		lb t2, 0(t1)
+		bne t2, zero, EP13
 			li a0, 1
 			mv ra, s6
 			ret
-	EP40:
+	EP13:
 	mv a0, zero
 	mv ra, s6
 	ret
-EP39:
-
-
-#########################################
-#a0 <- endereço da intenção do player
-#a1 <- endereço da movimentação do player
-#a2 <- endereço da posição do player
-#########################################
-
-enemyMovement:
-mv s7, ra
-mv s9, a0
-mv s10, a1
-mv s11, a2
-
-lb a0, 0(s9)
-mv a1, s11
-jal CheckEnemyMapCollision
-beq a0, zero, EP29
-	lb t0, 0(s9)
-	sb t0, 0(s10)
-	mv ra, s7
-	ret
-EP29:
-
-lb a0, 0(s10)
-mv a1, s11
-jal CheckEnemyMapCollision
-beq a0, zero, EP30
-	mv ra, s7
-	ret
-EP30:
-
-mv a1, s11
-lb a0, 0(s10)
-jal rotateClock
-beq a0, zero, EP31
-	lb a0, 0(s10)
-	jal rotateClock
-	sb a0, 0(s10)
-	mv ra, s7
-	ret
-EP31:
-
-mv a1, s11
-lb a0, 0(s10)
-jal rotateCounter
-beq a0, zero, EP32
-	lb a0, 0(s10)
-	jal rotateCounter
-	sb a0, 0(s10)
-	mv ra, s7
-	ret
-EP32:
-
-mv ra, s7
+EP9:
+mv ra, s6
 ret
 
-##############################
-#a0 <- endereço enemyMove
-#a1 <- endereço enemyPosition
+######################################################
+#a0 <- recebe endereço da posição da entidade
+#a1 <- recebe o endereço da ÚLTIMA posição da entidade
+#a2 <- recebe o endereço da movimentação da entidade
+######################################################
 
-##############################
-
-changeEnemyPos:
+changeEntityPos:
 mv s6, ra
-mv s9, a0
-mv s10, a1
-lb t0, 0(s9)
-lw t1, 0(s10)
-lw t2, 4(s10)
-
-bne t0, zero, EP41
-	addi t1, t1, 1
-	sw t1, 0(s10)
-	mv ra, s6
-	ret
-EP41: 
+lw t0, 0(a0)
+lw t1, 4(a0)
+sw t0, 0(a1)
+sw t1, 4(a1)
+lb t2, 0(a2)
+bne t2, zero, EP20
+	addi t0, t0, 1
+	sw t0, 0(a0)
+EP20:
 
 li t3, 1
-bne t0, t3, EP42
-	addi t2, t2, -1
-	sw t2, 4(s10)
-	mv ra, s6
-	ret
-EP42:
+bne t2, t3, EP21
+	addi t1, t1, -1
+	sw t1, 4(a0)
+EP21:
 
 li t3, 2
-bne t0, t3, EP43
-	addi t1, t1, -1
-	sw t1, 0(s10)
-	mv ra, s6
-	ret
-EP43:
+bne t2, t3, EP22
+	addi t0, t0, -1
+	sw t0, 0(a0)
+EP22:
 
 li t3, 3
-bne t0, t3, EP44
-	addi t2, t2, 1
-	sw t2, 4(s10)
+bne t2, t3, EP23
+	addi t1, t1, 1
+	sw t1, 4(a0)
+EP23:
+mv ra, s6
+ret
+
+############################################################
+#a0 <- endereço do primeiro pixel da tela/seção-de-memória
+#a1 <- posição x na tela
+#a2 <- posição y na tela
+#a3 <- cor a se renderizar
+############################################################
+
+solidColorTileRender:
+mv s6, ra
+li t3, 320
+li t4, 16
+mul s0, a2, t3
+add s0, s0, a1
+add s0, s0, a0 #endereço do primeiro pixel na tela
+
+li t1, 0
+LP6:
+	bge t1, t4, LE6
+	#---------------
+	li t2, 0
+	LP7:
+		bge t2, t4, LE7
+		#---------------
+		mul t5, t1, t3
+		add t5, t5, t2
+		add t5, t5, s0
+		sb a3, 0(t5)
+		#---------------
+		addi t2, t2, 1
+		jal LP7
+	LE7:
+	#---------------
+	addi t1, t1, 1
+	jal LP6
+LE6:
+
+mv ra, s6
+ret
+
+###########################################################
+#a0 <- endereço do primeiro pixel ta tela/região de memória
+#a1 <- posição x atual da entidade
+#a2 <- posição y atual da entidade
+#a3 <- última movimentação da entidade
+#a4 <- cor a se renderizar
+###########################################################
+
+moveEntityCollision:
+mv s6, ra
+li t3, 320
+li t4, 16
+mul s0, a2, t3
+add s0, s0, a1
+add s0, s0, a0 #endereço da entidade na tela
+
+bne a3, zero, EP31
+	addi s1, s0, 15 #primeiro pixel de escrita
+	addi s2, s0, -1 #primeiro pixel de apagamento
+	li t1, 0
+	LP8:
+		bge t1, t4, LE8
+		#--------------
+		mul t5, t1, t3 #contador*320
+		add t5, t5, s1 #(contador*320)+(enderço do primeiro pixel de escrita)
+		mul t6, t1, t3 #contador*320
+		add t6, t6, s2 #(contador*320)+(endereço do primeiro pixel de apagamento)
+		sb zero, 0(t6)
+		sb a4, 0(t5)
+		#--------------
+		addi t1, t1, 1
+		jal LP8
+	LE8:
+EP31:
+
+li t0, 1
+bne a3, t0, EP24
+	li s2, 5120
+	add s2, s2, s0 #endereço do primeiro pixel de apagamento
+	mv s1, s0 #endereço do primeiro pixel de escrita
+	li t1, 0
+	LP9:
+		bge t1, t4, LE9
+		#--------------
+		add t5, s1, t1
+		add t6, s2, t1
+		sb zero, 0(t6)
+		sb a4, 0(t5)
+		#--------------
+		addi t1, t1, 1
+		jal LP9
+	LE9:
+	
+EP24:
+
+li t0, 2
+bne a3, t0, EP29
+	mv s1, s0
+	addi s2, s0, 16
+	li t1, 0
+	LP10:
+		bge t1, t4, LE10
+		#---------------
+		mul t5, t1, t3
+		add t5, t5, s1
+		mul t6, t1, t3
+		add t6, t6, s2
+		sb zero, 0(t6)
+		sb a4, 0(t5)
+		#---------------
+		addi t1, t1, 1
+		jal LP10
+	LE10:
+EP29:
+
+li t0, 3
+bne a3, t0, EP30
+	addi s2, s0, -320 # endereço do primeiro pixel de apagamento
+	li s1, 4800
+	add s1, s1, s0 # endereço do primeiro pixel de escrita
+	li t1, 0
+	LP11:
+		bge t1, t4, LE11
+		#---------------
+		add t5, s1, t1
+		add t6, s2, t1
+		sb zero, 0(t6)
+		sb a4, 0(t5)
+		#---------------
+		addi t1, t1, 1
+		jal LP11
+	LE11:
+		
+EP30:
+mv ra, s6
+ret
+
+####################################################################################
+# você coloca um número (n) a0 e a função retorna (n-1) se (n > 3) e (3) se (n == 0)
+####################################################################################
+
+rotateClock:
+mv s6, ra
+bne a0, zero, EP18
+	li a0, 3
 	mv ra, s6
 	ret
-EP44:
+EP18:
+addi a0, a0, -1
+mv ra, s6
+ret
 
+
+####################################################################################
+# você coloca um número (n) a0 e a função retorna (n+1) se (n < 3) e (0) se (n == 3)
+####################################################################################
+
+rotateCounter:
+mv s6, ra
+li t0, 3
+bne t0, a0, EP19
+	mv a0, zero
+	mv ra, s6
+	ret
+EP19:
+addi a0, a0, 1
 mv ra, s6
 ret
